215cb82a41942727fe9a92b6e94072526de01cd4
Hanna Bicheuskaya <Hanna_Bicheuskaya@epam.com>
2023-04-28 10:33:54 +0400

    added RP and ci config

---
+++ src/ui/features/launchers-tab.feature
@@ -0,0 +1,5 @@
+Feature: Login user
+
+  Scenario: Login user with correct creds
+    Given I open "Login" page
+    And I should be on "Login" page and page is fully loaded
---
+++ src/ui/pages/page.ts
@@ -0,0 +1,30 @@
+import { waitForFullPageLoad } from "../../utils/browser/pageLoadWaiter";
+
+export default abstract class Page {
+  url = 'https://rp.epam.com/ui';
+  fullUrl = '/';
+
+  async open(url?: string) {
+    return url ? await browser.url(url) : await browser.url(this.fullUrl);
+  }
+
+  openNewTab() {
+    return browser.newWindow(this.fullUrl);
+  }
+
+  async waitForFullLoad(timeout?: number) {
+    await waitForFullPageLoad(timeout);
+  }
+
+  async waitForUrlToContain(text: string ) {
+    return !!(await browser.waitUntil(
+      async () =>
+          (await browser.getUrl()).indexOf(text) !== -1,
+      {
+        timeout: 30000,
+        timeoutMsg: `Actual URL: ${await browser.getUrl()}. \nExpected URL: ${text}`,
+      }
+    ));
+  }
+
+}
